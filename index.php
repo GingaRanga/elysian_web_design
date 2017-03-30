d4c267040d015b1f3b60f839745bfc570846f43c
Evan Marshall <evanmarshall@Evans-MacBook-Air.local>
2017-03-30 11:07:02 -0300

    first site commit. Navbar and Hero section almost complete.

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="">
+        
+        <title>Elysian Web Design | Beautiful Creations</title>
+        
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <!-- FontAwesome CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+        <!-- Main CSS including Bootstrap -->
+        <link href="css/app.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+       
+        <!-- NAVBAR ========================================================================================================================== -->
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bar-collapse" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand hidden-xs" href="/">
+                        <img src="img/banner-logo-2.svg" width="200" height="70" alt="Elysian Web Design Logo">
+                    </a>
+                    <a class="navbar-brand visible-xs" href="/">
+                        <img src="img/logo-blk.svg" width="70" height="70" alt="Elysian Web Design Logo">
+                    </a>
+                </div><!-- /.navbar-header -->
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+        
+        <!-- HERO ========================================================================================================================== -->
+        <section id="hero">
+            <article>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 text-wrap">
+                            <h1>Elysian Web Design</h1>
+                            <p class="lead">Our small town charm will guarantee that your experience is pleasurable from the fruition of your website design to it&#39;s world wide grand reveal!</p>
+                            <a href="#"><i class="fa fa-4x fa-circle" aria-hidden="true"></i></a>
+                        </div><!-- /#hero-text -->
+                    </div><!-- /.row -->
+                </div><!-- /.container -->
+            </article>
+        </section>
+        
+        <!-- PROPOSAL SECTION ================================================================================================================ -->
+        <section class="proposal">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8">
+                        <p class="lead"><strong>Request for proposal. </strong>Get your ideas online now!</p>
+                    </div><!-- /.col-sm-8 -->
+                    <div class="col-sm-4">
+                        <button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal1">Start request</button>
+                    </div><!-- /.col-sm-4 -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section>
+        
+        <!-- PLACEHOLDER TEXT ================================================================================================================ -->
+        <section>
+            <div class="container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, ut commodi doloribus voluptatibus vel consectetur voluptas eos nam corporis ducimus? Beatae architecto laboriosam earum iste deserunt! Temporibus, corporis impedit debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia hic magni, pariatur voluptatem nostrum ipsa nihil sunt expedita voluptate vitae aspernatur, provident sapiente sit ex dolore mollitia. Temporibus, assumenda, tempora. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum laudantium repellat commodi quidem dolor illo, voluptates maxime iste facilis aperiam neque magnam vitae animi praesentium voluptas natus, tempora. Dolores, dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor pariatur asperiores fugit eos, sint nulla ipsa quidem voluptate quaerat sequi commodi, sunt optio recusandae neque adipisci illo. Quo laudantium, accusamus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi neque saepe qui quod magnam quo temporibus magni maxime debitis consequuntur recusandae assumenda, cum quisquam doloremque, illum quidem tempore architecto quos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis cumque nisi unde delectus dolores, error architecto accusantium cupiditate amet eaque quia quae qui soluta commodi quos reiciendis hic libero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla tempora at deserunt enim fuga ipsam eos ex cupiditate numquam earum quod, nihil molestiae omnis repudiandae veritatis laudantium qui aliquid ducimus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, maiores error perspiciatis nihil magni impedit sed id quae corrupti molestias nesciunt, officia unde voluptas inventore sapiente est modi eos cupiditate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus facilis ullam dignissimos at quod delectus, incidunt labore iusto tempora reiciendis sint, placeat error a harum aliquid mollitia accusamus maxime laboriosam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam laudantium optio iusto velit tempora cupiditate magni voluptates, nam voluptatem quo eligendi blanditiis libero ex atque aspernatur, consectetur, quasi animi ipsa? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis eveniet, pariatur similique praesentium quas, id quam ut asperiores inventore in natus, ullam voluptas sit excepturi tenetur qui rerum recusandae veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias officia, magni at dicta obcaecati, ducimus dolorem aliquam libero natus odio veniam ut atque soluta recusandae? Accusantium nisi eaque ipsa rem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia ut hic in ipsa aliquam accusantium id magni eligendi, dignissimos reiciendis quis consequatur at similique, distinctio nobis harum veritatis perferendis autem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, ut commodi doloribus voluptatibus vel consectetur voluptas eos nam corporis ducimus? Beatae architecto laboriosam earum iste deserunt! Temporibus, corporis impedit debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia hic magni, pariatur voluptatem nostrum ipsa nihil sunt expedita voluptate vitae aspernatur, provident sapiente sit ex dolore mollitia. Temporibus, assumenda, tempora. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum laudantium repellat commodi quidem dolor illo, voluptates maxime iste facilis aperiam neque magnam vitae animi praesentium voluptas natus, tempora. Dolores, dolorem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor pariatur asperiores fugit eos, sint nulla ipsa quidem voluptate quaerat sequi commodi, sunt optio recusandae neque adipisci illo. Quo laudantium, accusamus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi neque saepe qui quod magnam quo temporibus magni maxime debitis consequuntur recusandae assumenda, cum quisquam doloremque, illum quidem tempore architecto quos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus blanditiis cumque nisi unde delectus dolores, error architecto accusantium cupiditate amet eaque quia quae qui soluta commodi quos reiciendis hic libero! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla tempora at deserunt enim fuga ipsam eos ex cupiditate numquam earum quod, nihil molestiae omnis repudiandae veritatis laudantium qui aliquid ducimus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, maiores error perspiciatis nihil magni impedit sed id quae corrupti molestias nesciunt, officia unde voluptas inventore sapiente est modi eos cupiditate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus facilis ullam dignissimos at quod delectus, incidunt labore iusto tempora reiciendis sint, placeat error a harum aliquid mollitia accusamus maxime laboriosam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam laudantium optio iusto velit tempora cupiditate magni voluptates, nam voluptatem quo eligendi blanditiis libero ex atque aspernatur, consectetur, quasi animi ipsa? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis eveniet, pariatur similique praesentium quas, id quam ut asperiores inventore in natus, ullam voluptas sit excepturi tenetur qui rerum recusandae veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias officia, magni at dicta obcaecati, ducimus dolorem aliquam libero natus odio veniam ut atque soluta recusandae? Accusantium nisi eaque ipsa rem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia ut hic in ipsa aliquam accusantium id magni eligendi, dignissimos reiciendis quis consequatur at similique, distinctio nobis harum veritatis perferendis autem!</p>
+            </div>
+        </section>
+        
+        <!-- PROPOSAL MODAL ================================================================================================================== -->
+        <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i> Message us</h4>
+                    </div><!-- /.modal-header -->
+                    <div class="modal-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem itaque nam velit culpa provident, harum voluptatem delectus nihil maxime suscipit eius sed deserunt nemo quos officiis dicta ut libero commodi.</p>
+                        <form>
+                            <div class="form-group form-group-lg">
+                                <label for="formName">Full name</label>
+                                <input type="text" class="form-control" id="formName" placeholder="Jane Doe">
+                            </div><!-- /.form-group -->
+                            <div class="form-group form-group-lg">
+                                <label for="formCompany">Company Name</label>
+                                <input type="text" class="form-control" id="formCompany" placeholder="Jane Doe Ltd">
+                            </div><!-- /.form-group -->
+                            <div class="form-group form-group-lg">
+                                <label for="formEmail">Email address</label>
+                                <input type="email" class="form-control" id="formEmail" placeholder="email@example.com">
+                            </div><!-- /.form-group -->
+                            <div class="form-group form-group-lg">
+                                <label for="formText">Project specifics</label>
+                                <textarea class="form-control" id="formText" rows="3" placeholder="Enter project specifics here..."></textarea>
+                            </div><!-- /.form-group -->
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            </div><!-- /.modal-footer -->
+                        </form>
+                    </div><!-- /.modal-body -->
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
+        <!-- PAGE SCRIPTS ==================================================================================================================== -->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
+        <!-- Custom JS -->
+        <script src="js/app.js"></script>
+        
+    </body>
+</html>
