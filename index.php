8670cec89ffac7fa2fba9fcd0c4d44b2ace48876
Evan Marshall <evanmarshall@Evans-MacBook-Air.local>
2017-04-05 15:41:39 -0300

    resized bg images for mobile, tweaked the social footer. Still some minor tweaks to do such as valign work descriptions then look into making site dynamic.

--- index.php
+++ index.php
@@ -166,7 +166,7 @@
                         <p>What is the point of having a beautiful website with lots of traffic&#44; but not knowing who or what type of client is driving said traffic&#63; Even more&#44; why wouldn&#39;t you want to know what is and what isn&#39;t working for your competition&#63; With Elysian&#39;s business solutions&#44; you will not only be in the known with similar companies&#44; but you will also know the specifics of who is viewing your website&#46; Wouldn&#39;t it be nice to know what parts of your site your viewers are clicking the most or what age the majority of viewers are&#63;</p>
                     </div>
                     <div class="col-sm-4 col-xs-12 service-opt">
-                        <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
+                        <i class="fa fa-facebook-official fa-3x" aria-hidden="true"></i>
                         <h3>Social Media Marketing</h3>
                         <p>If your websites doesn&#39;t have a presence on social media&#44; you are missing out on a <span class="uppercase">huge</span> opportunity&#33; My experience working with the top social media application program interfaces &#40;API&#41; will allow your website to be as integrated as you need&#46; As advanced as displaying your Facebook newsfeed on the site or a simple link&#45;to&#45;post on Twitter&#44; Elysian can make it happen&#33;</p>
                     </div>   
@@ -176,7 +176,7 @@
                         <p>There is nothing worse than clicking on a link to a local coffee shop and waiting for five minutes for the site to load&#46; Yes&#44; we are in the age of&#44; "two seconds is way too long&#33;" and so are your clients&#44; There is quite a bit that goes on&#44; "behind the scenes" to make those great sites load in an instant&#44; and I can make it happen&#46; Perhaps&#44; just as important as speed&#44; is making sure your website is found in a presentable manner on the worlds top search engines &#40;i&#46;e&#46; Google&#41;&#46; I will make sure your site is easily found as well as looking good when your viewers try to find you&#46;</p>
                     </div>
                 </div>
-                <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal1">Let&#39;s Chat</button>
+                <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal1">Begin Project</button>
             </div>
         </section>
         
@@ -434,45 +434,136 @@
                         <p>&copy; ELYSIAN WEB DESIGN 2017. All Rights Reserved.</p>
                     </div><!-- /.col-sm-6 -->
                     <div class="col-xs-12 col-sm-6 text-center">
-                        <a href="https://www.facebook.com/sharer/sharer.php?u=http://www.elysianwebdesign.com/" title="Share on Facebook" target="_blank" class="btn btn-facebook"><i class="fa fa-lg fa-facebook social-link"></i></a>
-                        <a href="http://twitter.com/home?status=Need%20a%20website%3F%20Check%20out%3A%20http://www.elysianwebdesign.com/" title="Share on Twitter" target="_blank" class="btn btn-twitter"><i class="fa fa-lg fa-twitter social-link"></i></a>
-                        <a href="http://www.linkedin.com/shareArticle?mini=true&url=http://www.elysianwebdesign.com&media=http://www.elysianwebdesign.com/img/facebook-mobile.png&title=Elysian%20Web%20Design&summary=Need%20a%20website%3F%20Check%20out%3A%20http://www.elysianwebdesign.com/" title="Share on LinkedIn" target="_blank" class="btn btn-linkedin"><i class="fa fa-lg fa-linkedin social-link"></i></a>
+                        <a href="https://www.facebook.com/sharer/sharer.php?u=http://www.elysianwebdesign.com/" title="Share on Facebook" target="_blank" class="btn btn-facebook"><i class="fa fa-lg fa-facebook"></i></a>
+                        <a href="http://twitter.com/home?status=Need%20a%20website%3F%20Check%20out%3A%20http://www.elysianwebdesign.com/" title="Share on Twitter" target="_blank" class="btn btn-twitter"><i class="fa fa-lg fa-twitter"></i></a>
+                        <a href="mailto:webdesignelysian@gmail.com?Subject=&#91;%20Company%20Name%20&#93;%20Has%20a%20message%20for%20Elysian%20Web%20Design&#33;&body=Please%20enter%20company%20name%20in%20email%20subject%20as%20well%20as%20your%20contact%20details%20and%20we%20will%20get%20back%20to%20you%20as%20soon%20as%20we%20can%20&#58;&#41;" target="_top" title="General Email" class="btn btn-email"><i class="fa fa-lg fa-envelope"></i></a>
+                        <a href="http://www.linkedin.com/shareArticle?mini=true&url=http://www.elysianwebdesign.com&media=http://www.elysianwebdesign.com/img/facebook-mobile.png&title=Elysian%20Web%20Design&summary=Need%20a%20website%3F%20Check%20out%3A%20http://www.elysianwebdesign.com/" title="Share on LinkedIn" target="_blank" class="btn btn-linkedin"><i class="fa fa-lg fa-linkedin"></i></a>
                     </div><!-- /.col-sm-6 -->
                 </div><!-- /.row -->
             </div><!-- /.container -->
         </div><!-- #social-wrap -->
         
         <!-- PROPOSAL MODAL ================================================================================================================= -->
-        <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal modal-full fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i> Message us</h4>
+                        <h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i> Request for Proposal</h4>
                     </div><!-- /.modal-header -->
                     <div class="modal-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem itaque nam velit culpa provident, harum voluptatem delectus nihil maxime suscipit eius sed deserunt nemo quos officiis dicta ut libero commodi.</p>
+                        <p>In order for Elysian to determine if we can provide you with the best solution&#44; please fill out as much detail as you can. If you have a general inquiry please email me at&#58; <a href="mailto:webdesignelysian@gmail.com?Subject=&#91;%20Company%20Name%20&#93;%20Has%20a%20message%20for%20Elysian%20Web%20Design&#33;&body=Please%20enter%20company%20name%20in%20email%20subject%20as%20well%20as%20your%20contact%20details%20and%20we%20will%20get%20back%20to%20you%20as%20soon%20as%20we%20can%20&#58;&#41;" target="_top">webdesignelysian@gmail.com</a></p>
                         <form>
-                            <div class="form-group form-group-lg">
-                                <label for="formName">Full name</label>
-                                <input type="text" class="form-control" id="formName" placeholder="Jane Doe">
-                            </div><!-- /.form-group -->
-                            <div class="form-group form-group-lg">
-                                <label for="formCompany">Company Name</label>
-                                <input type="text" class="form-control" id="formCompany" placeholder="Jane Doe Ltd">
-                            </div><!-- /.form-group -->
-                            <div class="form-group form-group-lg">
-                                <label for="formEmail">Email address</label>
-                                <input type="email" class="form-control" id="formEmail" placeholder="email@example.com">
-                            </div><!-- /.form-group -->
-                            <div class="form-group form-group-lg">
-                                <label for="formText">Project specifics</label>
-                                <textarea class="form-control" id="formText" rows="3" placeholder="Enter project specifics here..."></textarea>
-                            </div><!-- /.form-group -->
-                            <div class="modal-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            </div><!-- /.modal-footer -->
+                            <fieldset>
+                                <legend>Company / Personal Info</legend>
+                                <div class="form-group form-group-lg">
+                                    <label for="formName">Full name</label>
+                                    <input type="text" class="form-control" id="formName" placeholder="Jane Doe">
+                                </div><!-- /.form-group -->
+                                <div class="form-group form-group-lg">
+                                    <label for="formCompany">Company name</label>
+                                    <input type="text" class="form-control" id="formCompany" placeholder="Jane Doe Ltd">
+                                </div><!-- /.form-group -->
+                                <div class="form-group form-group-lg">
+                                    <label for="formTitle">Your position</label>
+                                    <input type="text" class="form-control" id="formTitle" placeholder="CTO, manager, president, etc.">
+                                </div><!-- /.form-group -->
+                                <div class="form-group form-group-lg">
+                                    <label for="formEmail">Email address</label>
+                                    <input type="email" class="form-control" id="formEmail" placeholder="email@example.com">
+                                </div><!-- /.form-group -->
+                                <div class="form-group form-group-lg">
+                                    <label for="formPhone">Phone number</label>
+                                    <input type="tel" class="form-control" id="formPhone" placeholder="XXX-XXX-XXXX">
+                                </div><!-- /.form-group -->
+                                <div class="form-group form-group-lg">
+                                    <label for="formURL">Website</label>
+                                    <input type="url" class="form-control" id="formURL" placeholder="www.example.com">
+                                </div><!-- /.form-group -->
+                                <hr>
+                                <legend>Project Info</legend>
+                                <div class="form-group form-group-lg">
+                                    <label for="formScope">Project scope</label>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Full website build
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Website revision
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Consultation
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">WordPress website
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-group form-group-lg">
+                                    <label for="formScope">&Aacute; la Carte Services</label>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Responsive website
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Pagespeed optimization
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Search engine optimization
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Brand &#45; logo design
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Analytics &amp; competitive analysis
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" value="">Social media marketing
+                                        </label>
+                                    </div>
+                                    <label for="formOther">Other</label>
+                                    <textarea class="form-control" id="formOther" rows="3" placeholder="Please state anything specific you would like to request for your webpage..."></textarea>
+                                </div>
+                                <div class="form-group form-group-lg">
+                                    <label for="formBudget">Your budget</label>
+                                    <select class="form-control">
+                                        <option value="1000"><&#36;1K</option>
+                                        <option value="2000">&#36;1K &#45; &#36;2K</option>
+                                        <option value="3000">&#36;2K &#45; &#36;3K</option>
+                                        <option value="4000">&#36;3K &#45; &#36;4K</option>
+                                        <option value="10000">>&#36;4K</option>
+                                        <option>no budget set</option>
+                                    </select>
+                                </div>
+                                <div class="form-group form-group-lg">
+                                    <label for="formText">Project specifics</label>
+                                    <textarea class="form-control" id="formText" rows="3" placeholder="Please state specific to your project, such as: timeline, goal of the project, do you want to manage future updates or not, a bit about your company..."></textarea>
+                                </div><!-- /.form-group -->
+                                <div class="form-group form-group-lg">
+                                    <label for="formRef">How did you hear about Elysian&#63;</label>
+                                    <textarea class="form-control" id="formRef" rows="3" placeholder="Please state how you heard about Elysian Web Design, be it: Google search, friend referral, etc..."></textarea>
+                                </div><!-- /.form-group -->
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div><!-- /.modal-footer -->
+                            </fieldset>
                         </form>
                     </div><!-- /.modal-body -->
                 </div><!-- /.modal-content -->
